208cb70673ec49a369c3560e4c84111119c930bd
Weslley Braga <wesbragagt@gmail.com>
2019-05-30 12:47:44 -0500

    click events working

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+class App extends Component {
+    state = {
+        todos: [
+            { id: 1, title: "take out the trash", completed: false },
+            { id: 2, title: "dinner with wife", completed: false },
+            { id: 3, title: "meeting with boss", completed: false }
+        ]
+    };
+    // Toggle Complete
+    
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo=>{
+            if(todo.id === id){
+                todo.completed = !todo.completed
+            }
+            return todo;
+        })})
+    };
+    // Delete todo
+    delTodo = (id)=>{
+        console.log(id);
+    }
+    render() {
+        console.log(this.state.todos);
+        return (
+            <Todos delTodo={this.delTodo} markComplete={this.markComplete} todos={this.state.todos} />
+        );
+    }
 }
 
 export default App;
