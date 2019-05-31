eb6a2519a5be042b111fcf6830bb293487f4fe4a
Weslley Braga <wesbragagt@gmail.com>
2019-05-31 12:34:07 -0500

    pushing the build folder

---
+++ reactapp/build/precache-manifest.3a1cceb88ed7c542150f54af828cfb9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2def5df2ca5d8d70bf8cc90c511fce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add0e4287c4445d33d2b",
+    "url": "/static/css/main.14b1f6fd.chunk.css"
+  },
+  {
+    "revision": "7ac9fe5dcd9eda254364",
+    "url": "/static/js/2.8b912063.chunk.js"
+  },
+  {
+    "revision": "add0e4287c4445d33d2b",
+    "url": "/static/js/main.80c9aa1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
