c5e10dd3cf2593c74c32e835b6d9158af234b715
Weslley Braga <wesbragagt@gmail.com>
2019-05-31 13:15:34 -0500

    Updates

---
+++ precache-manifest.06253fcc0e2c8cd7e2d945f1702c9ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0213425e65b9c17fa261ee83a461c57f",
+    "url": "/jamLog/index.html"
+  },
+  {
+    "revision": "5f32e3763aec1cc4533b",
+    "url": "/jamLog/static/css/main.56dcf84b.chunk.css"
+  },
+  {
+    "revision": "7ac9fe5dcd9eda254364",
+    "url": "/jamLog/static/js/2.8b912063.chunk.js"
+  },
+  {
+    "revision": "5f32e3763aec1cc4533b",
+    "url": "/jamLog/static/js/main.aae4dc61.chunk.js"
+  },
+  {
+    "revision": "196996ff465f6c656fee",
+    "url": "/jamLog/static/js/runtime~main.d9ae9953.js"
+  }
+]);
